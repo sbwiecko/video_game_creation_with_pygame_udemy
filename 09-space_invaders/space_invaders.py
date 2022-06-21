b3d7f68f01b0e2011269061c3324fe4bc106e2e7
Sébastien Wieckowski <swieckowski@worgpharma.com>
2022-06-21 16:37:29 +0200

    completed session 10

--- 09-space_invaders/space_invaders.py
+++ 09-space_invaders/space_invaders.py
@@ -1,25 +1,30 @@
 import pygame, random
 
-#Initialize pygame
+# Initialize pygame
 pygame.init()
 
-#Set display surface
-WINDOW_WIDTH = 1200
+# Set display surface
+WINDOW_WIDTH  = 1200
 WINDOW_HEIGHT = 700
-display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+display_surface = pygame.display.set_mode(
+    (WINDOW_WIDTH, WINDOW_HEIGHT)
+)
+BREACH_LINE = WINDOW_HEIGHT - 100 # bottom 'breach' line
+
 pygame.display.set_caption("Space Invaders")
 
-#Set FPS and clock
+# Set FPS and clock
 FPS = 60
 clock = pygame.time.Clock()
 
-#Define Classes
+
+# Define Classes
 class Game():
     """A class to help control and update gameplay"""
 
     def __init__(self, player, alien_group, player_bullet_group, alien_bullet_group):
-        """Initialze the game"""
-        #Set game values
+        """Initialize the game"""
+        # Set game values
         self.round_number = 1
         self.score = 0
 
@@ -28,27 +33,27 @@ class Game():
         self.player_bullet_group = player_bullet_group
         self.alien_bullet_group = alien_bullet_group
 
-        #Set sounds and music
-        self.new_round_sound = pygame.mixer.Sound("./space_invaders_assets/new_round.wav")
-        self.breach_sound = pygame.mixer.Sound("./space_invaders_assets/breach.wav")
-        self.alien_hit_sound = pygame.mixer.Sound("./space_invaders_assets/alien_hit.wav")
+        # Set sounds and music
+        self.new_round_sound  = pygame.mixer.Sound("./space_invaders_assets/new_round.wav")
+        self.breach_sound     = pygame.mixer.Sound("./space_invaders_assets/breach.wav")
+        self.alien_hit_sound  = pygame.mixer.Sound("./space_invaders_assets/alien_hit.wav")
         self.player_hit_sound = pygame.mixer.Sound("./space_invaders_assets/player_hit.wav")
 
-        #Set font
+        # Set font
         self.font = pygame.font.Font("./space_invaders_assets/Facon.ttf", 32)
 
     def update(self):
-        """Update the game"""
+        """Update the game in the main loop"""
         self.shift_aliens()
         self.check_collisions()
         self.check_round_completion()
 
     def draw(self):
         """Draw the HUD and other information to display"""
-        #Set colors
+        # Set colors
         WHITE = (255, 255, 255)
 
-        #Set text
+        # Set text
         score_text = self.font.render("Score: " + str(self.score), True, WHITE)
         score_rect = score_text.get_rect()
         score_rect.centerx = WINDOW_WIDTH//2
@@ -62,51 +67,53 @@ class Game():
         lives_rect = lives_text.get_rect()
         lives_rect.topright = (WINDOW_WIDTH - 20, 10)
 
-        #Blit the HUD to the display
+        # Blit the HUD to the display
         display_surface.blit(score_text, score_rect)
         display_surface.blit(round_text, round_rect)
         display_surface.blit(lives_text, lives_rect)
         pygame.draw.line(display_surface, WHITE, (0, 50), (WINDOW_WIDTH, 50), 4)
-        pygame.draw.line(display_surface, WHITE, (0, WINDOW_HEIGHT - 100), (WINDOW_WIDTH, WINDOW_HEIGHT - 100), 4)
+        pygame.draw.line(display_surface, WHITE, (0, BREACH_LINE), (WINDOW_WIDTH, BREACH_LINE), 4)
 
     def shift_aliens(self):
         """Shift a wave of aliens down the screen and reverse direction"""
-        #Determine if alien group has hit an edge
+        # Determine if alien group has hit an edge
         shift = False
         for alien in (self.alien_group.sprites()):
+            # check if any alien sprite touches the borders
             if alien.rect.left <= 0 or alien.rect.right >= WINDOW_WIDTH:
                 shift = True
 
-        #Shift every alien down, change direction, and check for a breach
+        # Shift every alien down, change direction, and check for a breach
         if shift:
+            # check for a breach only when shifting
             breach = False
             for alien in (self.alien_group.sprites()):
-                #Shift down
+                # Shift down
                 alien.rect.y += 10*self.round_number
 
-                #Reverse the direction and move the alien off the edge so 'shift' doesn't trigger
-                alien.direction = -1*alien.direction
+                # Reverse the direction and move the alien off the edge so 'shift' doesn't trigger
+                alien.direction *= -1
                 alien.rect.x += alien.direction*alien.velocity
 
-                #Check if an alien reached the ship
-                if alien.rect.bottom >= WINDOW_HEIGHT - 100:
+                # Check if an alien reached the ship
+                if alien.rect.bottom >= BREACH_LINE:
                     breach = True
             
-            #Aliens breached the line
+            # Aliens breached the line
             if breach:
                 self.breach_sound.play()
                 self.player.lives -= 1
                 self.check_game_status("Aliens breached the line!", "Press 'Enter' to continue")
 
-
     def check_collisions(self):
         """Check for collisions"""
-        #See if any bullet in the player bullet group hit an alien in the alien group
+        # See if any bullet in the player bullet group hit an alien in the alien group
+        # kill both sprites 'True', 'True', play a sound and increase the score
         if pygame.sprite.groupcollide(self.player_bullet_group, self.alien_group, True, True):
             self.alien_hit_sound.play()
             self.score += 100
         
-        #See if the player has collided with any bullet in the alien bullet group
+        # See if the player has collided with any bullet in the alien bullet group
         if pygame.sprite.spritecollide(self.player, self.alien_bullet_group, True):
             self.player_hit_sound.play()
             self.player.lives -= 1
@@ -115,67 +122,65 @@ class Game():
 
     def check_round_completion(self):
         """Check to see if a player has completed a single round"""
-        #If the alien group is empty, you've completed the round
+        # If the alien group is empty, you've completed the round
         if not (self.alien_group):
-            self.score += 1000*self.round_number
+            self.score += 1000*self.round_number # score bonus
             self.round_number += 1
             
             self.start_new_round()
 
-
     def start_new_round(self):
         """Start a new round"""
-        #Create a grid of Aliens 11 columns and 5 rows.  
+        # Create a grid of Aliens 11 columns and 5 rows.  
         for i in range(11):
             for j in range(5):
                 alien = Alien(64 + i*64, 64 + j*64, self.round_number, self.alien_bullet_group)
                 self.alien_group.add(alien)
 
-        #Pause the game and prompt user to start
+        # Pause the game and prompt user to start
         self.new_round_sound.play()
         self.pause_game("Space Invaders Round " + str(self.round_number), "Press 'Enter' to begin")
 
     def check_game_status(self, main_text, sub_text):
         """Check to see the status of the game and how the player died"""
-        #Empty the bullet groups and reset player and remaining aliens
+        # Empty the bullet groups and reset player and remaining aliens
         self.alien_bullet_group.empty()
         self.player_bullet_group.empty()
         self.player.reset()
         for alien in self.alien_group:
             alien.reset()
 
-        #Check if the game is over or if it is a simple round reset
+        # Check if the game is over or if it is a simple round reset
         if self.player.lives == 0:
             self.reset_game()
         else:
             self.pause_game(main_text, sub_text)
 
-
     def pause_game(self, main_text, sub_text):
         """Pauses the game"""
         global running
 
-        #Set colors
+        # Set colors
         WHITE = (255, 255, 255)
         BLACK = (0, 0, 0)
 
-        #Create main pause text
+        # Create main pause text
         main_text = self.font.render(main_text, True, WHITE)
         main_rect = main_text.get_rect()
         main_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2)
 
-        #Create sub pause text
+        # Create sub pause text
         sub_text = self.font.render(sub_text, True, WHITE)
         sub_rect = sub_text.get_rect()
         sub_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2 + 64)
 
-        #Blit the pause text
+        # Blit the pause text
         display_surface.fill(BLACK)
         display_surface.blit(main_text, main_rect)
         display_surface.blit(sub_text, sub_rect)
         pygame.display.update()
 
-        #Pause the game until the user hits enter
+        # Pause the game until the user hits enter
         is_paused = True
         while is_paused:
             for event in pygame.event.get():
@@ -192,18 +197,18 @@ class Game():
         """Reset the game"""
         self.pause_game("Final Score: " + str(self.score), "Press 'Enter' to play again")
 
-        #Reset game values
+        # Reset game values
         self.score = 0
         self.round_number = 1
 
         self.player.lives = 5
 
-        #Empty groups
+        # Empty groups
         self.alien_group.empty()
         self.alien_bullet_group.empty()
         self.player_bullet_group.empty()
 
-        #Start a new game
+        # Start a new game
         self.start_new_round()
 
 
@@ -229,7 +234,7 @@ class Player(pygame.sprite.Sprite):
         """Update the player"""
         keys = pygame.key.get_pressed()
 
-        #Move the player within the bounds of the screen
+        # Move the player within the bounds of the screen
         if keys[pygame.K_LEFT] and self.rect.left > 0:
             self.rect.x -= self.velocity
         if keys[pygame.K_RIGHT] and self.rect.right < WINDOW_WIDTH:
@@ -237,7 +242,7 @@ class Player(pygame.sprite.Sprite):
 
     def fire(self):
         """Fire a bullet"""
-        #Restrict the number of bullets on screen at a time
+        # Restrict the number of bullets on screen at a time
         if len(self.bullet_group) < 2:
             self.shoot_sound.play()
             PlayerBullet(self.rect.centerx, self.rect.top, self.bullet_group)
@@ -249,7 +254,7 @@ class Player(pygame.sprite.Sprite):
 
 class Alien(pygame.sprite.Sprite):
     """A class to model an enemy alien"""
-    
+
     def __init__(self, x, y, velocity, bullet_group):
         """Initialize the alien"""
         super().__init__()
@@ -270,7 +275,7 @@ class Alien(pygame.sprite.Sprite):
         """Update the alien"""
         self.rect.x += self.direction*self.velocity
 
-        #Randomly fire a bullet
+        # Randomly fire a bullet
         if random.randint(0, 1000) > 999 and len(self.bullet_group) < 3:
             self.shoot_sound.play()
             self.fire()
@@ -331,38 +336,41 @@ class AlienBullet(pygame.sprite.Sprite):
             self.kill()
 
 
-#Create bullet groups
-my_player_bullet_group = pygame.sprite.Group()
-my_alien_bullet_group = pygame.sprite.Group()
 
-#Create a player group and Player object
+# Create bullet groups
+my_player_bullet_group = pygame.sprite.Group()
+my_alien_bullet_group  = pygame.sprite.Group()
+
+# Create a player group and Player object
 my_player_group = pygame.sprite.Group()
 my_player = Player(my_player_bullet_group)
 my_player_group.add(my_player)
 
-#Create an alien group.  Will add Alien objects via the game's start new round method
+# Create an alien group.  Will add Alien objects via the game's start new round method
 my_alien_group = pygame.sprite.Group()
 
-#Create a Game object
+# Create a Game object
 my_game = Game(my_player, my_alien_group, my_player_bullet_group, my_alien_bullet_group)
 my_game.start_new_round()
 
-#The main game loop
+
+
+# The main game loop
 running = True
 while running:
-    #Check to see if the user wants to quit
+    # Check to see if the user wants to quit
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        #The player wants to fire
+        # The player wants to fire
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 my_player.fire()
 
-    #Fill the display
+    # Fill the display
     display_surface.fill((0, 0, 0))
 
-    #Update and display all sprite groups
+    # Update and display all sprite groups
     my_player_group.update()
     my_player_group.draw(display_surface)
 
@@ -375,13 +383,13 @@ while running:
     my_alien_bullet_group.update()
     my_alien_bullet_group.draw(display_surface)
 
-    #Update and draw Game object
+    # Update and draw Game object
     my_game.update()
     my_game.draw()
 
-    #Update the display and tick clock
+    # Update the display and tick clock
     pygame.display.update()
     clock.tick(FPS)
 
-#End the game
+# End the game
 pygame.quit()
--- 10-advanced_pygame_tutorial/advanced_pygame_tutorial.py
+++ 10-advanced_pygame_tutorial/advanced_pygame_tutorial.py
@@ -1,41 +1,45 @@
 import pygame
-#Use 2D vectors
+# Use 2D vectors
 vector = pygame.math.Vector2
 
-#Initialize pygame
+# Initialize pygame
 pygame.init()
 
-#Set display surface (tile size is 32x32 so 960/32 = 30 tiles wide, 640/32 = 20 tiles high)
-WINDOW_WIDTH = 960
+# Set display surface (tile size is 32x32 so 960/32 = 30 tiles wide, 640/32 = 20 tiles high)
+WINDOW_WIDTH  = 960
 WINDOW_HEIGHT = 640
-display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+display_surface = pygame.display.set_mode(
+    (WINDOW_WIDTH, WINDOW_HEIGHT)
+)
 pygame.display.set_caption("Making a tile map!")
 
-#Set FPS and clock
+# Set FPS and clock
 FPS = 60
 clock = pygame.time.Clock()
 
-#Define classes
+# Define classes
 class Tile(pygame.sprite.Sprite):
     """A class to read and create individual tiles and place them in the display"""
 
-    def __init__(self, x, y, image_int, main_group, sub_group=""):
+    def __init__(self, x, y, image_int, main_group, sub_group=""): # default no sub_group (e.g., dirt)
         super().__init__()
-        #Load in the correct image and add it to the correct sub groups
+        # Load in the correct image and add it to the correct sub groups
         if image_int == 1:
             self.image = pygame.image.load("./advanced_tutorial_assets/dirt.png")
         elif image_int == 2:
             self.image = pygame.image.load("./advanced_tutorial_assets/grass.png")
+            # to save computational resources while checking for collisions btw masks
+            # we create the grass mask only once, and it will be used whenever needed
             self.mask = pygame.mask.from_surface(self.image)
             sub_group.add(self)
         elif image_int == 3:
             self.image = pygame.image.load("./advanced_tutorial_assets/water.png")
             sub_group.add(self)
         
-        #Add every tile to the main tile group
+        # Add every tile to the main tile group
         main_group.add(self)
 
-        #Get the rect of the image and position within the grid
+        # Get the rect of the image and position within the grid
         self.rect = self.image.get_rect()
         self.rect.topleft = (x, y)
 
@@ -49,13 +53,14 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, x, y, grass_tiles, water_tiles):
         super().__init__()
 
-        #Animation Frames
+        # Animation Frames
         self.move_right_sprites = []
-        self.move_left_sprites = []
+        self.move_left_sprites  = []
         self.idle_right_sprites = []
-        self.idle_left_sprites = []
+        self.idle_left_sprites  = []
 
-        #Moving Right
+        # Moving Right
+        # We scale the image inline
         self.move_right_sprites.append(pygame.transform.scale(pygame.image.load("./advanced_tutorial_assets/boy/Run (1).png"), (64,64)))
         self.move_right_sprites.append(pygame.transform.scale(pygame.image.load("./advanced_tutorial_assets/boy/Run (2).png"), (64,64)))
         self.move_right_sprites.append(pygame.transform.scale(pygame.image.load("./advanced_tutorial_assets/boy/Run (3).png"), (64,64)))
@@ -65,11 +70,12 @@ class Player(pygame.sprite.Sprite):
         self.move_right_sprites.append(pygame.transform.scale(pygame.image.load("./advanced_tutorial_assets/boy/Run (7).png"), (64,64)))
         self.move_right_sprites.append(pygame.transform.scale(pygame.image.load("./advanced_tutorial_assets/boy/Run (8).png"), (64,64)))
 
-        #Moving Left
+        # Moving Left
         for sprite in self.move_right_sprites:
+            # same sprite list but flipped
             self.move_left_sprites.append(pygame.transform.flip(sprite, True, False))
 
-        #Idle Right
+        # Idle Right
         self.idle_right_sprites.append(pygame.transform.scale(pygame.image.load("./advanced_tutorial_assets/boy/Idle (1).png"), (64,64)))
         self.idle_right_sprites.append(pygame.transform.scale(pygame.image.load("./advanced_tutorial_assets/boy/Idle (2).png"), (64,64)))
         self.idle_right_sprites.append(pygame.transform.scale(pygame.image.load("./advanced_tutorial_assets/boy/Idle (3).png"), (64,64)))
@@ -79,8 +85,9 @@ class Player(pygame.sprite.Sprite):
         self.idle_right_sprites.append(pygame.transform.scale(pygame.image.load("./advanced_tutorial_assets/boy/Idle (7).png"), (64,64)))
         self.idle_right_sprites.append(pygame.transform.scale(pygame.image.load("./advanced_tutorial_assets/boy/Idle (8).png"), (64,64)))
 
-        #Idle Left
+        # Idle Left
         for sprite in self.idle_right_sprites:
+            # same sprite list but flipped
             self.idle_left_sprites.append(pygame.transform.flip(sprite, True, False))
 
         self.current_sprite = 0
@@ -94,26 +101,26 @@ class Player(pygame.sprite.Sprite):
         self.grass_tiles = grass_tiles
         self.water_tiles = water_tiles
 
-        #Kinematics vectors (first value is the x, second value is the y)
+        # Kinematics vectors (first value is the x, second value is the y)
         self.position = vector(x, y)
         self.velocity = vector(0, 0)
         self.acceleration = vector(0, 0)
 
-        #Kinematic constants
-        self.HORIZONTAL_ACCELERATION = 2
-        self.HORIZONTAL_FRICTION = 0.15
-        self.VERTICAL_ACCLERATION = .5 #Gravity
-        self.VERTICAL_JUMP_SPEED = 15 #Detemerines how high we can jump
+        # Kinematic constants
+        self.HORIZONTAL_ACCELERATION = 1
+        self.HORIZONTAL_FRICTION = 0.15 # otherwise player won't stop accelerating
+        self.VERTICAL_ACCLERATION = .5 # Gravity
+        self.VERTICAL_JUMP_SPEED = 13 # Detemerines how high we can jump
        
 
 
     def update(self):
         pygame.draw.rect(display_surface, (255, 255, 0), self.rect, 1)
         
-        #Create a mask
+        # Create a mask each time the image updates, i.e. player sprite changes
         self.mask = pygame.mask.from_surface(self.image)
-        #Draw the mask
-        mask_outline = self.mask.outline()
+        # Draw the mask
+        mask_outline = self.mask.outline() # gives a list of points/coordinates
         pygame.draw.lines(self.image, (255, 255, 0), True, mask_outline)
 
         self.move()
@@ -121,12 +128,12 @@ class Player(pygame.sprite.Sprite):
 
 
     def move(self):
-        #Set the accleration vector to (0, 0) so there is initially no acceleration
-        #If there is no force (no key presses) acting on the player then accerlation should be 0
-        #Vertical accelration (gravity) is present always regardless of key-presses
+        # Set the accleration vector to (0, 0) so there is initially no acceleration
+        # If there is no force (no key presses) acting on the player then accerlation should be 0
+        # Vertical accelration (gravity) is present always regardless of key-presses
         self.acceleration = vector(0, self.VERTICAL_ACCLERATION)
 
-        #If the user is presseing a key, set the x-component of the accleration vector to a non zero value.
+        # If the user is presseing a key, set the x-component of the accleration vector to a non zero value.
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
             self.acceleration.x = -1*self.HORIZONTAL_ACCELERATION
@@ -140,12 +147,12 @@ class Player(pygame.sprite.Sprite):
             else:
                 self.animate(self.idle_left_sprites, .2)
 
-        #Calculate new kinematics values (2, 5) + (1, 6) = (3, 11)
+        # Calculate new kinematics values (2, 5) + (1, 6) = (3, 11)
         self.acceleration.x -= self.velocity.x*self.HORIZONTAL_FRICTION
         self.velocity += self.acceleration
         self.position += self.velocity + 0.5*self.acceleration
 
-        #Update new rect based on kinematic calculations and add wrap around motion
+        # Update new rect based on kinematic calculations and add wrap around motion
         if self.position.x < 0:
             self.position.x = WINDOW_WIDTH
         elif self.position.x > WINDOW_WIDTH:
@@ -155,15 +162,19 @@ class Player(pygame.sprite.Sprite):
 
 
     def check_collisions(self):
-        #Check for collisions with the grass tiles
+        # Check for collisions with the grass tiles, so that the player is falling when from a grass tile
+        # only its mask doesn't collide with the grass rect, not the entire player rect
+        # Specify when the collision should be checked using mask -----------------------------------------v
         collided_platforms = pygame.sprite.spritecollide(self, self.grass_tiles, False, pygame.sprite.collide_mask)
+        # here the method also generates a mask for the grass tile
         if collided_platforms:
-            #Only move to the platform if the player is falling down
+            # Only move to the platform if the player is falling down
             if self.velocity.y > 0:
-                self.position.y = collided_platforms[0].rect.top + 10
+                self.position.y = collided_platforms[0].rect.top + 10 # fixing the jittering
                 self.velocity.y = 0
 
-        #Check for collisions with the water tiles
+        # Check for collisions with the water tiles
+        # The precision offered by mask is computational intensive and should not be used systematically
         if pygame.sprite.spritecollide(self, self.water_tiles, False):
             print("YOU CANT SWIM!!!")
             self.position = vector(self.starting_x, self.starting_y)
@@ -171,12 +182,12 @@ class Player(pygame.sprite.Sprite):
 
 
     def jump(self):
-        #Only jump if on a grass tile
+        # Only jump if on a grass tile
         if pygame.sprite.spritecollide(self, self.grass_tiles, False):
             self.velocity.y = -1*self.VERTICAL_JUMP_SPEED
 
     def animate(self, sprite_list, speed):
-        #Loop through the sprite list changing the current sprite
+        # Loop through the sprite list changing the current sprite
         if self.current_sprite < len(sprite_list) - 1:
             self.current_sprite += speed
         else:
@@ -185,14 +196,14 @@ class Player(pygame.sprite.Sprite):
         self.image = sprite_list[int(self.current_sprite)]
 
 
-#Create sprite groups
-main_tile_group = pygame.sprite.Group()
+# Create sprite groups
+main_tile_group  = pygame.sprite.Group()
 grass_tile_group = pygame.sprite.Group()
 water_tile_group = pygame.sprite.Group()
-my_player_group = pygame.sprite.Group()
+my_player_group  = pygame.sprite.Group()
 
-#Create the tile map: 0 -> no tile, 1 -> dirt, 2 -> grass, 3 -> water, 4 -> player
-#20 rows and 30 columns
+# Create the tile map: 0 -> no tile, 1 -> dirt, 2 -> grass, 3 -> water, 4 -> player
+# 20 rows and 30 columns tile map
 tile_map = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -213,13 +224,13 @@ tile_map = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 1, 1]
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 1, 1],
 ]
 
-#Create individual Tile objects from the tile map
-#Loop through the 20 lists in tile_map (i moves us down the map)
+# Create individual Tile objects from the tile map
+# Loop through the 20 lists in tile_map (i moves us down the map)
 for i in range(len(tile_map)):
-    #Loop through the 30 elements in a given list (j moves us across the map)
+    # Loop through the 30 elements in a given list (j moves us across the map)
     for j in range(len(tile_map[i])):
         if tile_map[i][j] == 1:
             Tile(j*32, i*32, 1, main_tile_group)
@@ -231,15 +242,15 @@ for i in range(len(tile_map)):
             my_player = Player(j*32, i*32 + 32, grass_tile_group, water_tile_group)
             my_player_group.add(my_player)
 
-#Load in a background image
+# Load in a background image
 background_image = pygame.image.load("./advanced_tutorial_assets/background.png")
 background_rect = background_image.get_rect()
 background_rect.topleft = (0, 0)
 
-#The main game loop
+# The main game loop
 running = True
 while running:
-    #Check to see if the user wants to quit
+    # Check to see if the user wants to quit
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -248,20 +259,20 @@ while running:
             if event.key == pygame.K_SPACE:
                 my_player.jump()
 
-    #Blit the background
+    # Blit the background
     display_surface.blit(background_image, background_rect)
 
-    #Draw tiles
+    # Draw tiles
     main_tile_group.draw(display_surface)
     main_tile_group.update()
 
-    #Update and draw sprites
+    # Update and draw sprites
     my_player_group.update()
     my_player_group.draw(display_surface)
 
-    #Update display and tick clock
+    # Update display and tick clock
     pygame.display.update()
     clock.tick(FPS)
 
-#End the game
+# End the game
 pygame.quit()
